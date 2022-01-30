7264f2597a43fdecc0b627aedcedb1c6dba9d21c
Ahmad ElMasri <ahmad.elmasri@ucalgary.ca>
2022-01-29 23:00:10 -0700

    Added widgets
    Appbar, FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_one/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -13,15 +14,33 @@ class AppOne extends StatefulWidget {
 
 class _AppOneState extends State<AppOne> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  void onClick() {
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'AppOne',
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark(),
         home: Scaffold(
-          appBar: null,
-          body: null,
-          floatingActionButton: null,
+          appBar: AppBar(
+            title: const Text('AppOne'),
+            centerTitle: true,
+            backgroundColor: AppColors.mintGreen,
+          ),
+          body: SafeArea(
+            child: Container(),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.account_tree_outlined),
+            onPressed: onClick,
+          ),
           resizeToAvoidBottomInset: true,
         ));
   }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const royalBlue = Color(0xFF246EE9);
+  static const scarletRed = Color(0xFFFF2400);
+  static const mintGreen = Color(0xFF3EB489);
+}
